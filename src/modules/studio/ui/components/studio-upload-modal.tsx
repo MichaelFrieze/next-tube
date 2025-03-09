880ab4611590dd3b347726ac7c859d4432329c3f
Michael Frieze <michaelfrieze@icloud.com>
2025-03-08 21:54:28 -0500

    feat: add Mux

--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -1,11 +1,17 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { trpc } from "@/trpc/client";
-import { Loader2Icon, PlusIcon } from "lucide-react";
 import { toast } from "sonner";
+// import { useRouter } from "next/navigation";
+import { Loader2Icon, PlusIcon } from "lucide-react";
+
+import { trpc } from "@/trpc/client";
+import { Button } from "@/components/ui/button";
+import { ResponsiveModal } from "@/components/responsive-modal";
+
+import { StudioUploader } from "./studio-uploader";
 
 export const StudioUploadModal = () => {
+  // const router = useRouter();
   const utils = trpc.useUtils();
   const create = trpc.videos.create.useMutation({
     onSuccess: () => {
@@ -17,18 +23,38 @@ export const StudioUploadModal = () => {
     },
   });
 
+  // const onSuccess = () => {
+  //   if (!create.data?.video.id) return;
+
+  //   create.reset();
+  //   router.push(`/studio/videos/${create.data.video.id}`);
+  // };
+
   return (
-    <Button
-      variant={"secondary"}
-      onClick={() => create.mutate()}
-      disabled={create.isPending}
-    >
-      {create.isPending ? (
-        <Loader2Icon className="animate-spin" />
-      ) : (
-        <PlusIcon />
-      )}
-      Create
-    </Button>
+    <>
+      <ResponsiveModal
+        title="Upload a video"
+        open={!!create.data?.url}
+        onOpenChange={() => create.reset()}
+      >
+        {create.data?.url ? (
+          <StudioUploader endpoint={create.data.url} onSuccess={() => {}} />
+        ) : (
+          <Loader2Icon />
+        )}
+      </ResponsiveModal>
+      <Button
+        variant="secondary"
+        onClick={() => create.mutate()}
+        disabled={create.isPending}
+      >
+        {create.isPending ? (
+          <Loader2Icon className="animate-spin" />
+        ) : (
+          <PlusIcon />
+        )}
+        Create
+      </Button>
+    </>
   );
 };
